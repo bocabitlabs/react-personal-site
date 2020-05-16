d43625d7c7be50a21443a17d653ce6bfc1a769d5
renefs <renefs87@gmail.com>
2020-05-16 08:55:56 +0000

    Deploying to gh-pages from  @ 06886462715b0aa854ceb5a8e8d19edcaa01bd5e 🚀

---
+++ precache-manifest.2e93a0c32ef48821fa47c93a51701f87.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8b66f2f8aa06da55b7fc62a9bb1cade",
+    "url": "/index.html"
+  },
+  {
+    "revision": "04c1ad68d7af2970f8e8",
+    "url": "/static/css/main.c88aa4c2.chunk.css"
+  },
+  {
+    "revision": "31422c92858ac703b389",
+    "url": "/static/js/2.80720b80.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.80720b80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04c1ad68d7af2970f8e8",
+    "url": "/static/js/main.a3984389.chunk.js"
+  },
+  {
+    "revision": "e6262a922abb87c4beaa",
+    "url": "/static/js/runtime-main.eda09e61.js"
+  }
+]);
