35eff20118568aa376179dd6a43c23de2816572c
René Fernández <renefs87@gmail.com>
2020-05-16 15:29:23 +0200

    Load other js/css and imgs from same repo

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -5,15 +5,15 @@ function Carousel() {
     <div
       id="header-big-imgs"
       data-num-img="5"
-      data-img-src-1="https://static.renefernandez.com/static/img/somiedo.jpg"
+      data-img-src-1="%PUBLIC_URL%/img/somiedo.jpg"
       data-img-desc-1="Somiedo, Asturias"
-      data-img-src-2="https://static.renefernandez.com/static/img/luarca.jpg"
+      data-img-src-2="%PUBLIC_URL%/img/luarca.jpg"
       data-img-desc-2="Luarca, Asturias"
-      data-img-src-3="https://static.renefernandez.com/static/img/ginebra.jpg"
+      data-img-src-3="%PUBLIC_URL%/img/ginebra.jpg"
       data-img-desc-3="Geneva, Switzerland"
-      data-img-src-4="https://static.renefernandez.com/static/img/aviles.jpg"
+      data-img-src-4="%PUBLIC_URL%/img/aviles.jpg"
       data-img-desc-4="Avilés, Asturias"
-      data-img-src-5="https://static.renefernandez.com/static/img/londres.jpg"
+      data-img-src-5="%PUBLIC_URL%/img/londres.jpg"
       data-img-desc-5="London, UK"
     />
   );
